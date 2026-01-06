b0a0ccde82cd588589bf463e8fd3f0093eb70329
anaahnb <alvncar@gmail.com>
2026-01-05 23:58:52 -0300

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <DefaultLayout>
     <RouterView />
+    <Footer />
   </DefaultLayout>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import Footer from './components/Footer.vue';
 
 defineOptions({ shadow: true });
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <p>
+      Design baseado no  © Letterboxd.
+      Feitor por <a href="https://github.com/anaahnb?tab=repositories">Ana Alencar</a>. Dados dos filmes por <a href="https://www.themoviedb.org/">TMDB</a>.
+    </p>
+  </footer>
+</template>
+
+<style lang="scss">
+  footer {
+    background-color: #2c3440;
+    padding: .5rem 0;
+    display: flex;
+    justify-content: center;
+    color: #678;
+    font-size: .75rem;
+    margin-top: 3rem;
+
+    p {
+      margin: 0;
+    }
+
+    a {
+      color: #678;
+    }
+  }
+</style>
