a8c5753c610d4ff0add0e04490daa1c7fa95b6c2
anaahnb <alvncar@gmail.com>
2026-01-10 13:06:47 -0300

    feat: loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    :class="$style.loading">
+    <VueSpinnerFacebook
+      :size="46"
+      color="#9ab" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { VueSpinnerFacebook } from 'vue3-spinners';
+
+</script>
+
+<style lang="scss" module>
+.loading {
+  display: flex;
+  justify-content: center;
+  height: 84vh;
+  margin-top: 1rem;
+}
+</style>
